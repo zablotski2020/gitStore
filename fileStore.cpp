6093a73a4ad5a3056ae5353c9e19a00c80c271f5
zablotski2020 <72109229+zablotski2020@users.noreply.github.com>
2020-10-18 23:20:55 +0300

    New code

---
+++ fileStore.cpp
@@ -0,0 +1,7 @@
+#include "fileStore.hpp"
+#include <iostream>
+using namespace std;
+
+int main{
+    cout << "Potato" << endl;
+}
---
+++ fileStore.hpp
@@ -0,0 +1,6 @@
+#ifndef fileStore_hpp
+#define fileStore_hpp
+
+#include <stdio.h>
+
+#endif /* fileStore_hpp */
